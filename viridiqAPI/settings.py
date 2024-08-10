524317d50d21551828379b8d27422dfa68be3407
Mbishu2002 <fmbishu@gmail.com>
2024-08-10 21:52:48 +0100

    update

--- viridiqAPI/settings.py
+++ viridiqAPI/settings.py
@@ -146,7 +146,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Static Files Configuration (if needed)
 STATIC_URL = '/static/'
-STATICFILES_ROOT = [os.path.join(BASE_DIR, 'staticfiles')]
+STATIC_ROOT = [os.path.join(BASE_DIR, 'staticfiles')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
